2f3c2d5aa79e705a0632a01fb97796a20343168f
Debabrata24 <mr.debabrtapc2006@gmail.com>
2026-08-03 00:11:53 +0530

    oth

--- back/server.js
+++ back/server.js
@@ -18,7 +18,7 @@ app.use(cors({
 if (process.env.YT_COOKIES_BASE64) {
     const cookiesContent = Buffer.from(process.env.YT_COOKIES_BASE64, 'base64').toString('utf-8');
     fs.writeFileSync('./yt_cookies.txt', cookiesContent);
-    console.log('cookies.txt created successfully');
+    console.log('yt_cookies.txt created successfully');
 }
 
 
